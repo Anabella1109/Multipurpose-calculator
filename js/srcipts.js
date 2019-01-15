b6f9eb69dd381686a8e81bf4ebadf49adc75e248
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-01-15 16:13:56 +0200

    add section for keyboard input

--- js/srcipts.js
+++ js/srcipts.js
@@ -8,3 +8,82 @@ var memory = [[]];        //variable to store input as array so they be turned i
     errorMessage = "Something went wrong.";     
     output = document.getElementById("output");                //variable to display number entered before operator
     outputOperations = document.getElementById("outputOperations");        //variable to display number entered after operator
+
+
+    //Additional Feature: keyboard input
+window.addEventListener("keydown", function (event) {
+  if (event.defaultPrevented) {
+    return; // Do nothing if the event was already processed
+  }
+  switch (event.key) {
+    case "0":
+    collectMemory(0);
+    break;
+    case "1":
+    collectMemory(1);
+    break;
+    case "2":
+    collectMemory(2);
+    break;
+    case "3":
+    collectMemory(3);
+    break;
+    case "4":
+    collectMemory(4);
+    break;
+    case "5":
+    collectMemory(5);
+    break;
+    case "6":
+    collectMemory(6);
+    break;
+    case "7":
+    collectMemory(7);
+    break;
+    case "8":
+    collectMemory(8);
+    break;
+    case "9":
+    collectMemory(9);
+    break;
+    // Operator Keys
+    case "+":
+    operator("+");
+    break;
+    case "-":
+    operator("-");
+    break;
+    case "/":
+    operator("/");
+    break;
+    case "*":
+    operator("*");
+    break;
+    // Dot or Comma Value
+    case ".":
+    collectMemoryDot(".");
+    break;
+    case ",":
+    collectMemoryDot(".");
+    break;
+    // Enter or Equal
+    case "Enter":
+    calculate();
+    break;
+    case "=":
+    calculate();
+    break;
+    // Backspace = delete last input
+    case "Backspace":
+    ce();
+    break;
+    // Escape = AC
+    case "Escape":
+    ac();
+    break;
+    default:
+    return; // Quit when this doesn't handle the key event.
+  }
+  // Cancel the default action to avoid it being handled twice
+  event.preventDefault();
+}, true);
