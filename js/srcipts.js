de2fa11b826803c3299865e0fa521b7523678f7e
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-01-15 16:11:27 +0200

    add comments explaining the use of variables

--- js/srcipts.js
+++ js/srcipts.js
@@ -1,10 +1,10 @@
 // Define variables
-var memory = [[]];
-    result = 0;
-    memoryIndex = 0;
+var memory = [[]];        //variable to store input as array so they be turned into string to be compared with expressions
+    result = 0;         //variable to store result
+    memoryIndex = 0;      //variable to help navigate through array
     dotCount = 0;      //variable to keep track of number of dots to avoid user using more than one dot
     operatorCount = 0;       //variable to keep track of operators to avoid user inserting two operators at once
-    numCount = 1;            
-    errorMessage = "Something went wrong.";
-    output = document.getElementById("output");
-    outputOperations = document.getElementById("outputOperations");
+    numCount = 1;            //variable to keep track of the numbers passed to an operator
+    errorMessage = "Something went wrong.";     
+    output = document.getElementById("output");                //variable to display number entered before operator
+    outputOperations = document.getElementById("outputOperations");        //variable to display number entered after operator
