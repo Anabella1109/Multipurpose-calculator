0d78a797b9cb77c63334dae41bb16e31e6fcd6c5
TUYISENGE Anabella <bellaxbx1109@gmail.com>
2019-01-17 18:13:10 +0200

    re-evaluate business logic

--- js/scripts.js
+++ js/scripts.js
@@ -17,38 +17,38 @@ $(document).ready(function(event) {
   event.preventDefault();
 });
 
-function converter(source,valNum) {
-  valNum = parseFloat(valNum);
+function converter(source,inputValue) {
+  inputValue = parseFloat(inputValue);
   
   if (source=="inputPounds") {
-    inputKilograms.value=(valNum/2.2046).toFixed(4);
-    inputOunces.value=(valNum*16).toFixed(4);
-    inputGrams.value=(valNum/0.0022046).toFixed(4);
-    inputStones.value=(valNum*0.071429).toFixed(4);
+    inputtedKilograms.value=(inputValue/2.2046).toFixed(4);
+    inputtedOunces.value=(inputValue*16).toFixed(4);
+    inputtedGrams.value=(inputValue/0.0022046).toFixed(4);
+    inputtedStones.value=(inputValue*0.071429).toFixed(4);
   }
-  if (source=="inputKilograms") {
-    inputPounds.value=(valNum*2.2046).toFixed(4);
-    inputOunces.value=(valNum*35.274).toFixed(4);
-    inputGrams.value=(valNum*1000).toFixed(4);
-    inputStones.value=(valNum*0.1574).toFixed(4);
+  if (source=="inputtedKilograms") {
+    inputPounds.value=(inputValue*2.2046).toFixed(4);
+    inputtedOunces.value=(inputValue*35.274).toFixed(4);
+    inputtedGrams.value=(inputValue*1000).toFixed(4);
+    inputtedStones.value=(inputValue*0.1574).toFixed(4);
   }
-  if (source=="inputOunces") {
-    inputPounds.value=(valNum*0.062500).toFixed(4);
-    inputKilograms.value=(valNum/35.274).toFixed(4);
-    inputGrams.value=(valNum/0.035274).toFixed(4);
-    inputStones.value=(valNum*0.0044643).toFixed(4);
+  if (source=="inputtedOunces") {
+    inputPounds.value=(inputValue*0.062500).toFixed(4);
+    inputtedKilograms.value=(inputValue/35.274).toFixed(4);
+    inputtedGrams.value=(inputValue/0.035274).toFixed(4);
+    inputtedStones.value=(inputValue*0.0044643).toFixed(4);
   }
-  if (source=="inputGrams") {
-    inputPounds.value=(valNum*0.0022046).toFixed(4);
-    inputKilograms.value=(valNum/1000).toFixed(4);
-    inputOunces.value=(valNum*0.035274).toFixed(3);
-    inputStones.value=(valNum*0.00015747).toFixed(5);
+  if (source=="inputtedGrams") {
+    inputPounds.value=(inputValue*0.0022046).toFixed(4);
+    inputtedKilograms.value=(inputValue/1000).toFixed(4);
+    inputtedOunces.value=(inputValue*0.035274).toFixed(3);
+    inputtedStones.value=(inputValue*0.00015747).toFixed(5);
   }
-  if (source=="inputStones") {
-    inputPounds.value=(valNum*14).toFixed(4);
-    inputKilograms.value=(valNum/0.15747).toFixed(4);
-    inputOunces.value=(valNum*224).toFixed(4);
-    inputGrams.value=(valNum/0.00015747).toFixed(4);
+  if (source=="inputtedStones") {
+    inputPounds.value=(inputValue*14).toFixed(4);
+    inputtedKilograms.value=(inputValue/0.15747).toFixed(4);
+    inputtedOunces.value=(inputValue*224).toFixed(4);
+    inputtedGrams.value=(inputValue/0.00015747).toFixed(4);
   }
 
 }
